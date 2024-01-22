e9d1b9932d0aef40beb51dc4508b548aec5d119a
gh0st1608 <egalindoa@uni.pe>
2024-01-22 15:40:58 -0500

    fix validator card number

--- Backend/src/module/interfaces/http/card.schema.ts
+++ Backend/src/module/interfaces/http/card.schema.ts
@@ -3,19 +3,9 @@ import joi  from "joi";
 import luhn from "fast-luhn";
 export const cardSchema = {
     CARD: {
-        /* headers: joi.object({
-            authorization: joi.string().pattern(new RegExp(/^pk_test_[a-zA-Z0-9]{16}$/)),//Joi.string().pattern(/^pk_test_[a-zA-Z0-9]{16}$/),
-            }), */
-        /* headers: joi.object({
-                authorization: joi.string().pattern(/^pk_test_[a-zA-Z0-9]{4}$/),
-                'postman-token': joi.string().allow()
-            }), */
         body: joi.object({
-            card_number: joi.number().required().custom((value : any) => {
-                const bol = (value !== undefined && luhn(value.toString()) )&& (value.toString().length >= 13 && value.toString().length <= 16)
-/*                 console.log(bol) */
-                return bol
-                //return helper.message("Password must be at least 8 characters long");
+            card_number: joi.number().required().custom((value : any, helpers) => {
+                return ( (value !== undefined && luhn(value.toString()) ) && (value.toString().length > 13 && value.toString().length <= 16) ) ? true : helpers.error("any.invalid") 
             }),
             cvv: joi.number().required().custom((value : any, helpers) => {
                 return (value !== undefined) && (value.toString().length == 3 || value.toString().length === 4) ? true : helpers.error("any.invalid")
@@ -31,10 +21,7 @@ export const cardSchema = {
                 return (isNaN(value)) ? helpers.error("any.invalid") : (numericMonth >= 1 && numericMonth <= 12 ) ? true : helpers.error("any.invalid")
             }),
             email: joi.string().required().email().min(5).max(100).custom((value : any, helpers) => {
-                console.log(value)
                 const validDomains = ["gmail.com", "hotmail.com", "yahoo.es"];
-                console.log(!value)
-                console.log(validDomains.includes(value.split("@")[1]));
                 return (!value) ?  helpers.error("any.invalid") : (validDomains.includes(value.split("@")[1])) ? true : helpers.error("any.invalid")
             }),
         }),
