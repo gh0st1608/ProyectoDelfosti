0dfdecd288e141d5f0b9b4750fad296293fe0f74
gh0st1608 <egalindoa@uni.pe>
2024-01-22 15:22:49 -0500

    fix validators

--- Backend/src/module/interfaces/http/card.schema.ts
+++ Backend/src/module/interfaces/http/card.schema.ts
@@ -12,28 +12,30 @@ export const cardSchema = {
             }), */
         body: joi.object({
             card_number: joi.number().required().custom((value : any) => {
-                return (value !== undefined && luhn(value.toString()) )&& (value.toString().length >= 13 && value.toString().length <= 16)
+                const bol = (value !== undefined && luhn(value.toString()) )&& (value.toString().length >= 13 && value.toString().length <= 16)
+/*                 console.log(bol) */
+                return bol
                 //return helper.message("Password must be at least 8 characters long");
             }),
-            cvv: joi.number().required().custom((value : any) => {
-                return value !== undefined && (value.toString().length === 3 || value.toString().length === 4)
+            cvv: joi.number().required().custom((value : any, helpers) => {
+                return (value !== undefined) && (value.toString().length == 3 || value.toString().length === 4) ? true : helpers.error("any.invalid")
             }),
-            expiration_year: joi.string().length(4).required().custom((value : any) => {
+            expiration_year: joi.string().length(4).required().custom((value : any, helpers) => {
                 const numericYear = Number(value)
                 if (isNaN(numericYear)) return false;
                 const todayYear = (new Date()).getFullYear()
-                return numericYear >= todayYear && numericYear <= todayYear + 5
+                return (numericYear >= todayYear && numericYear <= todayYear + 5) ? true : helpers.error("any.invalid")
             }),
-            expiration_month: joi.string().min(1).max(2).required().custom((value : any) =>{
+            expiration_month: joi.string().min(1).max(2).required().custom((value : any, helpers) =>{
                 const numericMonth = Number(value)
-                if (isNaN(numericMonth)) return false;
-                return numericMonth >= 1 && numericMonth <= 12;
+                return (isNaN(value)) ? helpers.error("any.invalid") : (numericMonth >= 1 && numericMonth <= 12 ) ? true : helpers.error("any.invalid")
             }),
-            email: joi.string().required().email().min(5).max(100).custom((value : any) => {
+            email: joi.string().required().email().min(5).max(100).custom((value : any, helpers) => {
+                console.log(value)
                 const validDomains = ["gmail.com", "hotmail.com", "yahoo.es"];
-                if (!value) return false;
-                const domain = value.split("@")[1];
-                return validDomains.includes(domain);
+                console.log(!value)
+                console.log(validDomains.includes(value.split("@")[1]));
+                return (!value) ?  helpers.error("any.invalid") : (validDomains.includes(value.split("@")[1])) ? true : helpers.error("any.invalid")
             }),
         }),
     }
